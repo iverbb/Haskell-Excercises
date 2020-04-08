1599b9dc4d969a1cb3ee2af03cfc5586afc7a952
Iver Brønstad <iverbb@stud.ntnu.no>
2020-04-09 04:06:06 +0800

    bugfixes

--- zeroes.hs
+++ zeroes.hs
@@ -1,30 +1,23 @@
 module Zeroes where
 
-import Data.List
+import Data.List (genericLength, group, nub)
 
-factorize :: Integral a => a -> a -> [a]
-factorize 0 _ = []
-factorize 1 _ = []
-factorize 2 _ = [2]
-factorize n m
-  | m > ceiling (sqrt $ fromIntegral n)
-  = [n]
-  | b == 0
-  = [m] <> factorize a m
-  | otherwise
-  = factorize n (m+1)
-  where (a,b) = divMod n m
+factorize :: Integral a => a -> [a]
+factorize n 
+  = case f of
+      [] -> [n]
+      _  -> f <> factorize (div n (head f))
+  where f = take 1 $ filter (\x -> mod n x == 0) [2..n-1]
 
+multipliers :: Integral a => a -> ([a], [a])
+multipliers n = (nub fs, genericLength <$> group fs)
+  where fs = factorize n
 
---multipliers :: Integral a => a -> ([a], [a])
-multipliers n = (nub fs, length <$> (group fs))
-  where fs = factorize n 2
-
---allExps :: Integral a => a -> a -> a -> a
+allExps :: Integral a => a -> a -> a -> a
 allExps n f c = sum [div n (f^x) | x <- [1..c]]
 
 
---zeroes :: Integral a => a -> a -> a
+zeroes :: Integral a => a -> a -> a
 zeroes 0 _ = 0
 zeroes b n = minimum $ zipWith (div) (zipWith (allExps n) fs cs) cs
 	where (fs, cs) = multipliers b
